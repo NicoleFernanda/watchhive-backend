b655024021647d8ac029a693ed186795583e7012
NicoleFernanda <nicf2012@gmail.com>
2025-10-12 03:20:13 -0300

    ajustes choreo

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,11 @@ export UV_CACHE_DIR="/tmp/uv_cache"
 cd /app
 
 echo "Rodando migracoes Alembic."
-/app/.venv/bin/python3 -m alembic upgrade head
+/app/.venv/bin/alembic upgrade head 
 echo "Migracoes concluídas."
 
 # 2. Inicia a aplicação (Substituindo 'poetry run uvicorn')
 # Usamos 'exec' para garantir que o script seja substituído pelo processo do servidor.
 # Adapte este comando para o SEU formato de inicialização:
-exec uv run fastapi run main.py --port 8000 --host 0.0.0.0
+exec /app/.venv/bin/uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1
+# Chama o binário uvicorn diretamente.
