460e0f4fec08992a9e42b89529f8e7ecb0269ae1
NicoleFernanda <nicf2012@gmail.com>
2025-11-02 10:58:16 -0300

    ajuste cors

--- main.py
+++ main.py
@@ -31,12 +31,13 @@ app.include_router(review_router)
 app.include_router(follows_router)
 app.include_router(user_list_router)
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=['*'],
-#     allow_methods=['*'],
-#     allow_headers=['Authorization', 'Content-Type', '*'],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.get_allowed_origins(),
+    allow_credentials=True,
+    allow_methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allow_headers=['Authorization', 'Content-Type', 'Accept'],
+)
 
 @app.get('/')
 async def read_root():
